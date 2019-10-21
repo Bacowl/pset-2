9036b69f3f3ec9da46e952523900dafb894d98e0
Benjamin Carpenter <BCarpenter@ucvts.tec.nj.us>
2019-10-21 13:15:33 -0400

    initial commit

--- src/hello-user.js
+++ src/hello-user.js
@@ -1,4 +1,4 @@
 const readlineSync = require("readline-sync");
 
-const name = readlineSync.question("What is your name? ");
-console.log("Hello, " + name + "!");
+const name = readlineSync.question("\nWhat is your name?\n");
+console.log("\nHello," + name + "!");
