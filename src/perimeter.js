140f1c8011006e9f2a6b9a8653eb06f0d05eab1e
Benjamin Carpenter <BCarpenter@ucvts.tec.nj.us>
2019-10-23 13:26:04 -0400

    first commit

--- src/perimeter.js
+++ src/perimeter.js
@@ -3,8 +3,8 @@ const readlineSync = require("readline-sync");
 const Width = readlineSync.question("\nWidth: ");
 const Length = readlineSync.question("Length: ");
 const conv = 2.54;
-let Area = 2* Width+2*Length*conv;
-let Deci = Math.round(100*Area)/(100)
+let Peri= 2* Width+2*Length*conv;
+let Deci = Math.round(100*Peri)/(100)
 let Final = Deci.toLocaleString();
 
 console.log("\nA(n) " + Width + "-by-" + Length + "-inch sheet of paper has a perimeter of " + (Final) + " centimeter(s)")
