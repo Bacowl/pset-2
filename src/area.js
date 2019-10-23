dd2afd63ea5168f3ac931ccdf2463000a6def10c
Benjamin Carpenter <BCarpenter@ucvts.tec.nj.us>
2019-10-23 12:08:03 -0400

    first commit

---
+++ src/area.js
@@ -0,0 +1,10 @@
+const readlineSync = require("readline-sync");
+
+const Width = readlineSync.question("\nWidth: ");
+const Length = readlineSync.question("Length: ");
+const conv = 645.16;
+let Area = Width*Length*conv;
+let Deci = Math.round(100*Area)/(100)
+let Final = Deci.toLocaleString();
+
+console.log("\nA(n) " + Width + "-by-" + Length + "-inch sheet of paper has an area of " + (Final) + " square millimeter(s)")
